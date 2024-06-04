88c7ed77dbcc6523767834e2666267be3bb0cd3e
Valentin Ricard <me@valentin.red>
2024-06-04 18:54:50 +0200

    feat: Added  reflector

--- scripts/setup-flux.sh
+++ scripts/setup-flux.sh
@@ -4,6 +4,7 @@ echo "[...] Setting up flux..."
 
 ## Note: We want for the service to use SSH instead of HTTPS (github tokens expires FAST)
 flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
   --token-auth=false \
   --owner=aostrom-dev \
   --repository=cld \
